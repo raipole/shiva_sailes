0877e686517a5b9dbf36bf9ee89e36324c3fc220
SAILS <sails@IBAB-Workshop-Comp12.ibab.ac.in>
2026-04-24 11:24:54 +0530

    shiva_sailes

---
+++ m.py
@@ -0,0 +1,20 @@
+def compries():
+    n=str(input('enter the word:'))
+    c=''
+    for i in n:
+        a=1
+        while n[i]==n[i+1]:
+            a+=1
+        b=n[i]+str(a)
+       
+        if n[i]!=n[i+1]:
+   
+        
+        c+=k
+    c+=b
+    print(c)
+	   
+	
+	        
+if __name__ == '__main__':
+	compries()
