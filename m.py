ec8c1de940bb9f2c3d2be0fc48525bc93fbf2c00
SAILS <sails@IBAB-Workshop-Comp12.ibab.ac.in>
2026-04-24 12:14:22 +0530

    prime_number

--- m.py
+++ m.py
@@ -1,3 +1,4 @@
+
 def compries():
     n=str(input('enter the word:'))
     c=''
@@ -5,13 +6,11 @@ def compries():
         a=1
         while n[i]==n[i+1]:
             a+=1
-        b=n[i]+str(a)
+        b=n[i]+(a)
        
         if n[i]!=n[i+1]:
-   
-        
-        c+=k
-    c+=b
+            c+=k
+            c+=b
     print(c)
 	   
 	
