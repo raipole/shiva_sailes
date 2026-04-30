bdf9d01d63cc97bad1695c5d258cc62fba4ee94f
shiva <99349294+raipole@users.noreply.github.com>
2026-04-30 14:36:16 +0530

    Update list_kemers.py

--- list_kemers.py
+++ list_kemers.py
@@ -1,11 +1,11 @@
-# def k_mers():
-#     lis=list(input('enter list:'))
-#     k=int(input('enter k:'))
-#     lis_k=[i for i in lis if lis.count(i)==k]
-#     print(set(lis_k))
-#
-# if __name__=='__main__':
-#     k_mers()
+ def k_mers():
+     lis=list(input('enter list:'))
+     k=int(input('enter k:'))
+     lis_k=[i for i in lis if lis.count(i)==k]
+     print(set(lis_k))
+
+ if __name__=='__main__':
+     k_mers()
 #Write a program to remove all occurrences of an element from a list,
 def remov():
     lis=list(input('enter list:'))
